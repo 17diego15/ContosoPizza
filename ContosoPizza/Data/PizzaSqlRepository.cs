43ec2fe6afcb145f0ef9b1badb5a28289a7b7345
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-01-26 20:11:46 +0100

    Pizzas completo

--- ContosoPizza/Data/PizzaSqlRepository.cs
+++ ContosoPizza/Data/PizzaSqlRepository.cs
@@ -18,7 +18,32 @@ public class PizzaSqlRepository : IPizzaRepository
 
     public List<Pizza> GetAll()
     {
-        throw new NotImplementedException();
+        var pizzas = new List<Pizza>();
+
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            connection.Open();
+
+            var sqlString = "SELECT PizzaId, Nombre, Precio, IsGluten FROM Pizza";
+            var command = new SqlCommand(sqlString, connection);
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var pizza = new Pizza
+                    {
+                        Id = Convert.ToInt32(reader["PizzaId"]),
+                        Name = reader["Nombre"].ToString(),
+                        Price = Convert.ToDecimal(reader["Precio"]),
+                        IsGlutenFree = Convert.ToBoolean(reader["IsGluten"])
+                    };
+                    pizzas.Add(pizza);
+                }
+            }
+        }
+
+        return pizzas;
     }
 
     public Pizza? Get(int id)
@@ -41,7 +66,7 @@ public class PizzaSqlRepository : IPizzaRepository
                         Id = Convert.ToInt32(reader["PizzaId"]),
                         Name = reader["Nombre"].ToString(),
                         Price = Convert.ToDecimal(reader["Precio"]),
-                        IsGlutenFree = Convert.ToBoolean(reader["IsGluten"])    
+                        IsGlutenFree = Convert.ToBoolean(reader["IsGluten"])
                         //Balance = (decimal)reader[2]
                     };
                 }
@@ -54,16 +79,57 @@ public class PizzaSqlRepository : IPizzaRepository
 
     public void Add(Pizza pizza)
     {
-        throw new NotImplementedException();
-    }
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            connection.Open();
 
+            var sqlString = "INSERT INTO Pizza (Nombre, Precio, IsGluten) VALUES (@Nombre, @Precio, @IsGluten)";
+            var command = new SqlCommand(sqlString, connection);
+
+            command.Parameters.AddWithValue("@Nombre", pizza.Name);
+            command.Parameters.AddWithValue("@Precio", pizza.Price);
+            command.Parameters.AddWithValue("@IsGluten", pizza.IsGlutenFree ? 1 : 0);
+
+            command.ExecuteNonQuery();
+        }
+    }
+    
     public void Delete(int id)
     {
-        throw new NotImplementedException();
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            connection.Open();
+
+            var sqlString = "DELETE FROM Pizza WHERE PizzaId = @PizzaId";
+            var command = new SqlCommand(sqlString, connection);
+
+            command.Parameters.AddWithValue("@PizzaId", id);
+
+            command.ExecuteNonQuery(); 
+        }
     }
+
 
     public void Put(Pizza pizza)
     {
-        throw new NotImplementedException();
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            connection.Open();
+
+            var sqlString = @"
+            UPDATE Pizza 
+            SET Nombre = @Nombre, Precio = @Precio, IsGluten = @IsGluten 
+            WHERE PizzaId = @PizzaId";
+
+            using (var command = new SqlCommand(sqlString, connection))
+            {
+                command.Parameters.AddWithValue("@PizzaId", pizza.Id);
+                command.Parameters.AddWithValue("@Nombre", pizza.Name);
+                command.Parameters.AddWithValue("@Precio", pizza.Price);
+                command.Parameters.AddWithValue("@IsGluten", pizza.IsGlutenFree ? 1 : 0);
+
+                int rowsAffected = command.ExecuteNonQuery();
+            }
+        }
     }
 }
